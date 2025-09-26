b791f4f17f9aa11495fd1dc00e7031bbc0844b3e
Diego Engelhard <diego.engelhard1@gmail.com>
2025-09-26 15:22:06 -0600

    remove duplicate files

--- test-02/backend/src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import express, { Request, Response } from 'express';
-const router = express.Router();
-
-/* GET home page. */
-router.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript!');
-});
-
-export default router;
