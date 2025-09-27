73ce17f0090ccea0b092dec5654c21ec6037625e
Diego Engelhard <diego.engelhard1@gmail.com>
2025-09-26 21:00:42 -0600

    add: setup client

--- test-01/backend/src/routes/index.ts
+++ test-01/backend/src/routes/index.ts
@@ -6,9 +6,4 @@ import rateLimiterRouter from "./rateLimiter.router"
 // Mount the rate limiter router
 router.use('/rate-limiter', rateLimiterRouter);
 
-/* GET home page. */
-router.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript!');
-});
-
 export default router;
--- test-01/frontend/src/pages/Home.tsx
+++ test-01/frontend/src/pages/Home.tsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import Temp from '../components/Temp'
+import React from "react";
+import Temp from "../components/Temp";
 
 const Home = () => {
-    return (
-        <>
-            <h1>Home</h1>
-            <Temp />
-        </>
-    )
-}
+  return (
+    <>
+      <div className="min-h-screen bg-slate-800 flex items-center justify-center">
+        <div className="bg-emerald-500 p-8 rounded-xl shadow-lg text-center">
+          <h1 className="text-4xl font-bold text-white underline decoration-wavy">
+            ¡Tailwind Funciona! 🚀
+          </h1>
+          <p className="text-white mt-4">
+            ¡todo está configurado correctamente!
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
